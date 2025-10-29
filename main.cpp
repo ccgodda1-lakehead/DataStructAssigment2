5438881db77c3b96675823971a3d41a112213ab1
ccgodda1-lakehead <ccgodda1@lakeheadu.ca>
2025-10-29 14:44:48 -0400

    small touch ups

--- main.cpp
+++ main.cpp
@@ -23,30 +23,41 @@ int main() {
 
 	cout << "Starting List: " << mainDLL << endl;
 
+	//Section A Task 1
 	cout << "Amount of 0 changed to -1: " << replaceVal(mainDLL, 0, -1) << endl;
 
 	cout << "Changed List: " << mainDLL << endl;
 
+	//Section A task 2
 	cout << "Removed Negatives: " << mainDLL.rmv_ret_negative() << endl;
 
 	cout << "Changed List: " << mainDLL << endl;
 
+	//Section A task 3
 	int remove = 5;
 
 	mainDLL.RemoveAllDuplicatesOf(remove);
 
-	cout << "Removing dups of 5: " << mainDLL << endl;
+	cout << "Removing duplicates of 5: " << mainDLL << endl;
 
 	//getting random variable
 	srand(time(0));
 
 	int ranIndex = rand() % mainDLL.getSize();
 
+	//Section A task 4
 	cout << "getting variable at index " << ranIndex << ": [" << mainDLL.get_at(ranIndex)->get_val() << "]" << endl;
 
 	int value = 200;
 
+	//Section A task 5
 	mainDLL.insert_before(ranIndex, value);
 
-	cout << "Inserting before that index: " << mainDLL << endl;
+	cout << "Inserting 200 before that index: " << mainDLL << endl;
+
+
+	cout << endl << "-------------------" << endl;
+
+
+	//stack testing here.
 }
